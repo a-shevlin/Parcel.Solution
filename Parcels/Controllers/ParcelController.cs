3d6a17d5d62138039042e7ece11df337c812b8d7
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-19 14:28:20 -0700

    initial commit
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>

---
+++ Parcels/Controllers/ParcelController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CarDealership.Models;
+
+namespace Parcel.Controllers
+{
+  public class ParcelsController : Controller
+  {
+  }
+}
